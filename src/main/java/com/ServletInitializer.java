baaa07927174e62fc156b49cb6223b10b510621e
dongqi <374761060@qq.com>
2023-07-14 22:42:48 +0800

    master commit2

--- src/main/java/com/ServletInitializer.java
+++ src/main/java/com/ServletInitializer.java
@@ -8,7 +8,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         System.out.println("hello master");
-        System.out.println("hello hot-fix123");
         return application.sources(ShopApplication.class);
     }
 
